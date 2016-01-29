7e392ec113ec1fd2bccb3699c2bd15f02628f497
Erik Allik <eallik@gmail.com>
2016-01-29 15:40:18 +0200

    wip: generalize IOArrow to Control.Arrow (but I should instead use Kleisli)

--- src/Intrusive.hs
+++ src/Intrusive.hs
@@ -2,10 +2,14 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 
 module Intrusive where
 
+import           Control.Arrow
+import           Control.Category
 import           Control.Monad (forever, void)
+import           Prelude       hiding ((.))
 
 data ShouldTrace = Trace | NoTrace
 type Traceable a = (ShouldTrace, a)
@@ -14,23 +18,48 @@ noTrace :: a -> Traceable a
 trace   = \x -> (Trace      , x)
 noTrace = \x -> (NoTrace    , x)
 
-
 data IOArrow :: * -> * -> * where
+  IOId    :: IOArrow a a
+  IOConst :: b -> IOArrow a b
+  IOStage :: (a -> IO b) -> IOArrow a b
   IOPipe  :: forall a b c. (a -> IO c) -> (c -> IO b) -> IOArrow a b
   IOForev :: forall a    . IOArrow a a                -> IOArrow a a
 
-(>>>)   = IOPipe
+(>>>) = IOPipe
 ioForev = IOForev
 
-runIOPipe  :: IOArrow a  b  ->  a -> IO       b
-runIOPipe' :: IOArrow () b        -> IO       b
+instance Functor (IOArrow a) where
+  fmap f IOId         = IOStage (return . f)
+  fmap f (IOStage g)  = IOStage (fmap f . g)
+  fmap f (IOPipe g h) = undefined
+  fmap f (IOForev _)  = undefined
 
-runIOPipe (IOPipe  a b ) = \x -> a x  >>= b
-runIOPipe (IOForev pipe) = \x -> forever $ runIOPipe pipe x
+instance Applicative (IOArrow a) where
+  pure x    = IOConst x
+  af <*> ax = IOStage $ \a -> do f <- runIOArrow af a
+                                 x <- runIOArrow ax a
+                                 return (f x)
 
-runIOPipe' x = runIOPipe x ()
+instance Category IOArrow where
+  id = IOId
+  f . g = IOStage $ \b -> runIOArrow g b >>= runIOArrow f
 
-runIOPipe_  ::  IOArrow  a  b  ->  a -> IO                   ()
-runIOPipe'_ ::  IOArrow  () b        -> IO                   ()
-runIOPipe_  pipe x = void $ runIOPipe pipe x
-runIOPipe'_ pipe   = void $ runIOPipe pipe ()
+instance Arrow IOArrow where
+  arr f = IOStage (return . f)
+  first a = IOStage $ \(b, d) -> runIOArrow a b >>= return . (,d)
+
+runIOArrow  :: IOArrow a  b  ->  a -> IO       b
+runIOArrow IOId           = return
+runIOArrow (IOStage  f)   = \x   -> f x
+runIOArrow (IOPipe  a b ) = \x -> a x  >>= b
+runIOArrow (IOForev pipe) = \x -> forever $ runIOArrow pipe x
+
+runIOArrow' :: IOArrow () b        -> IO       b
+runIOArrow' x = runIOArrow x ()
+
+runIOArrow_  ::  IOArrow  a  b  ->  a -> IO                   ()
+runIOArrow_' ::  IOArrow  () b        -> IO                   ()
+runIOArrow_  pipe x = void $ runIOArrow pipe x
+runIOArrow_' pipe   = void $ runIOArrow pipe ()
+
+runIOPipe_' = runIOArrow_'  -- don't break Lib.hs
